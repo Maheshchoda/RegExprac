1a761030741839855e9958311b3ddbd01f9c75ad
Maheshchoda <choda.maheshbabu@gmail.com>
2018-08-05 11:28:49 +0530

    Validate frontEnd with RegEx

--- formValidation/formValidation.js
+++ formValidation/formValidation.js
@@ -1,1 +1,22 @@
-const regex = //
+const inputs = document.querySelectorAll('input');
+
+const patterns = {
+  telephone : /^\d{11}$/,
+  username  : /^\D{5,12}$/,
+  password  : /^[\w@-]{8,20}$/,
+  email     : /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,3})$/,
+}
+
+const validate = (field, regex) => {
+  if(regex.test(field.value)) {
+    field.className = 'valid';
+  }else {
+    field.className = 'notValid';
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener('keyup', (e) => {
+    validate(e.target, patterns[e.target.attributes.name.value]);//(grab input, name value from form)
+  });
+});
